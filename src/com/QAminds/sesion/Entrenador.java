37280618cb1d08ef4c7bd101a100ee36dfcfe3a7
ziriako <ziriako@gmail.com>
2021-08-28 11:25:07 -0500

    Sesion 6

---
+++ src/com/QAminds/sesion/Entrenador.java
@@ -0,0 +1,9 @@
+package com.QAminds.sesion;
+
+public class Entrenador {
+
+    public void hacerEjercicio(){
+        System.out.println("Haz ejercicio");
+    }
+
+}
---
+++ src/com/QAminds/sesion/Main.java
@@ -0,0 +1,11 @@
+package com.QAminds.sesion;
+import java.util.*;
+
+public class Main {
+    public static void main(String []args){
+
+        Entrenador musculoso = new Entrenador();
+        musculoso.hacerEjercicio();
+        System.out.println("Mucho gusto");
+    }
+}
